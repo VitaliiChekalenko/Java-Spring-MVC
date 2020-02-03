82774945e56dcc2947b75abfac7dfea8c9d79244
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-02-03 13:56:39 +0200

    second try

---
+++ src/main/java/badtwin/RestApi.java
@@ -0,0 +1,13 @@
+package badtwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApi.class);
+
+    }
+}
